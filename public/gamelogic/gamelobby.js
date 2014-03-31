eaea3ab9e6f2557016438bff70c1d8416953d503
DerKorb <hackin@gmx.de>
2014-04-01 00:05:27 +0200

    tested client side code

--- public/gamelogic/gamelobby.js
+++ public/gamelogic/gamelobby.js
@@ -2,8 +2,12 @@ var GameLobby = function()
 {
 	this.illustrate = function(changes)
 	{
-		//if (changes.host == me)
-		this.html.find("#start").hide();
+		console.log(me, changes.host);
+		if (changes.host != me)
+			this.html.find("#start").hide();
+		else
+			this.html.find("#start").show();
+
 		if (changes.host)
 			this.html.find("li[html~='"+changes.host+"']").css("color", "blue");
 	};
--- public/gamelogic/lobby.js
+++ public/gamelogic/lobby.js
@@ -1,9 +1,7 @@
+var me = "DerKorb";
 Lobby = function(object)
 {
-	// inherit events handler
-	this.template = "body->#Lobby>h1{GameLobby Lobby &type}+button#create{create new game}+ul#game_list";
-	this.type = "Lobby";
-
-	// inherit network object
-    client.networkObject.call(this, object);
+	this.on("login", function(data) {
+		me = data;
+	});
 }
